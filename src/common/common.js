42a6473f8f96fedd17578344751c1a64482f2b0b
dingyle <dingyle@yonyou.com>
2018-08-03 14:17:19 +0800

    添加changeDate方法

--- src/common/common.js
+++ src/common/common.js
@@ -2,4 +2,7 @@ exports.install = function(Vue, options) {
     Vue.prototype.changeData = function(msg) {
         alert(msg);
     };
+    Vue.prototype.changeDate = function(msg) {
+        alert('1111' + msg);
+    };
 };
